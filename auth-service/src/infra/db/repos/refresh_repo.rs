0384f9c72eec2bb0354b0dd15eeee121422258e5
Evgeniy <121779561+NightRunnerEB@users.noreply.github.com>
2025-09-12 23:54:17 +0300

    RefreshRepo implementation

--- auth-service/src/infra/db/repos/refresh_repo.rs
+++ auth-service/src/infra/db/repos/refresh_repo.rs
@@ -1,62 +1,97 @@
-// use async_trait::async_trait;
-// use chrono::{DateTime, Utc};
-// use sqlx::{Pool, Postgres, prelude::FromRow};
+use async_trait::async_trait;
+use sqlx::{PgPool, Pool, Postgres};
 
-// use crate::auth_core::{errors::AuthError, models::*, ports::RefreshRepo};
+use crate::{
+    auth_core::{errors::AuthError, models::*, ports::RefreshRepo},
+    infra::repos::RefreshRow,
+};
 
-// #[derive(Clone)]
-// pub struct SqlxRefreshRepo { pub pool: Pool<Postgres> }
+#[derive(Clone)]
+pub struct PgRefreshRepo {
+    pub pool: Pool<Postgres>,
+}
 
-// #[async_trait]
-// impl RefreshRepo for SqlxRefreshRepo {
-//     async fn get_by_hash(&self, hash: &[u8]) -> Result<Option<RefreshRecord>, AuthError> {
-//         let r = sqlx::query_as!(
-//             RefreshRow,
-//             r#"
-//             SELECT jti, user_id, session_id, family_id, expires_at, rotated_at, revoked_at
-//             FROM refresh_tokens
-//             WHERE token_hash = $1
-//             "#,
-//             hash
-//         )
-//         .fetch_optional(&self.pool)
-//         .await
-//         .map_err(|e| AuthError::Storage(e.to_string()))?;
+impl PgRefreshRepo {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
 
-//         Ok(r.map(|x| RefreshRecord {
-//             jti: x.jti, user_id: x.user_id, session_id: x.session_id, family_id: x.family_id,
-//             expires_at: x.expires_at, rotated_at: x.rotated_at, revoked_at: x.revoked_at
-//         }))
-//     }
+#[async_trait]
+impl RefreshRepo for PgRefreshRepo {
+    async fn get_by_hash(&self, hash: &[u8]) -> Result<Option<RefreshToken>, AuthError> {
+        let row = sqlx::query_as!(
+            RefreshRow,
+            r#"
+            SELECT jti, user_id, session_id, expires_at, rotated_at, revoked_at
+            FROM refresh_tokens
+            WHERE token_hash = $1
+            "#,
+            hash
+        )
+        .fetch_optional(&self.pool)
+        .await
+        .map_err(|e| AuthError::Storage(format!("refresh.get_by_hash: {e}")))?;
 
-//     async fn mark_rotated(&self, jti: Uid) -> Result<(), AuthError> {
-//         sqlx::query!(r#"UPDATE refresh_tokens SET rotated_at = now() WHERE jti = $1"#, jti)
-//             .execute(&self.pool).await
-//             .map_err(|e| AuthError::Storage(e.to_string()))?;
-//         Ok(())
-//     }
+        Ok(match row {
+            None => None,
+            Some(r) => Some(r.try_into()?),
+        })
+    }
 
-//     async fn insert(&self, rec: NewRefresh) -> Result<(), AuthError> {
-//         sqlx::query!(
-//             r#"
-//             INSERT INTO refresh_tokens
-//               (jti, user_id, session_id, token_hash, family_id, parent_jti, expires_at)
-//             VALUES ($1, $2, $3, $4, $5, $6, $7)
-//             "#,
-//             rec.jti, rec.user_id, rec.session_id, rec.token_hash, rec.family_id, rec.parent_jti, rec.expires_at
-//         )
-//         .execute(&self.pool).await
-//         .map_err(|e| AuthError::Storage(e.to_string()))?;
-//         Ok(())
-//     }
+    async fn mark_rotated(&self, jti: Uid) -> Result<bool, AuthError> {
+        let res = sqlx::query!(
+            r#"
+            UPDATE refresh_tokens 
+            SET rotated_at = now() 
+            WHERE jti = $1
+                AND rotated_at IS NULL
+                AND revoked_at is NULL
+                AND expires_at > now()
+            "#,
+            jti
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(|e| AuthError::Storage(format!("refresh.mark_rotated: {e}")))?;
 
-//     async fn revoke_family(&self, family_id: Uid) -> Result<(), AuthError> {
-//         sqlx::query!(
-//             r#"UPDATE refresh_tokens SET revoked_at = now() WHERE family_id = $1 AND revoked_at IS NULL"#,
-//             family_id
-//         )
-//         .execute(&self.pool).await
-//         .map_err(|e| AuthError::Storage(e.to_string()))?;
-//         Ok(())
-//     }
-// }
+        Ok(res.rows_affected() == 1)
+    }
+
+    async fn insert(&self, rec: NewRefresh) -> Result<(), AuthError> {
+        sqlx::query!(
+            r#"
+            INSERT INTO refresh_tokens (jti, user_id, session_id, token_hash, parent_jti, expires_at)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            "#,
+            rec.jti,
+            rec.user_id,
+            rec.session_id,
+            rec.token_hash,
+            rec.parent_jti,
+            rec.expires_at
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(|e| AuthError::Storage(format!("refresh.insert: {e}")))?;
+
+        Ok(())
+    }
+
+    async fn revoke_all_for_session(&self, session_id: Uid) -> Result<(), AuthError> {
+        sqlx::query!(
+            r#"
+            UPDATE refresh_tokens 
+            SET revoked_at = now() 
+            WHERE session_id = $1 
+                AND revoked_at IS NULL
+            "#,
+            session_id
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(|e| AuthError::Storage(format!("refresh.revoke_all_for_session: {e}")))?;
+
+        Ok(())
+    }
+}
